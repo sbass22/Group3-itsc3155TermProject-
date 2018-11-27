b0356e90d0830ad105428b4cfb91c22e5d38b48d
Han <hnguye82@uncc.edu>
2018-11-27 20:15:36 +0000

    Adding associated comments model

--- Recipes/db/schema.rb
+++ Recipes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_151420) do
+ActiveRecord::Schema.define(version: 2018_11_27_194740) do
 
   create_table "articles", force: :cascade do |t|
     t.string "Recipe_Name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_10_26_151420) do
     t.text "Directions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
