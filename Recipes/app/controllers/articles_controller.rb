15d5f4b12916bd3f3888b977c4f18732a0571641
Han <hnguye82@uncc.edu>
2018-11-27 17:36:19 +0000

    Add form partial and update the new view

--- Recipes/app/controllers/articles_controller.rb
+++ Recipes/app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        
+
         if @article.save
             redirect_to @article
         else
