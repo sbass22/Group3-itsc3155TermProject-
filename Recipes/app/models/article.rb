a4253f71d7c85ea150f5092f446a399bea124da9
Han <hnguye82@uncc.edu>
2018-11-27 20:49:06 +0000

    Adding destroy functionality for comments

--- Recipes/app/models/article.rb
+++ Recipes/app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     validates :Recipe_Name, presence: true, length: {minimum: 3}
     validates :Ingredients_and_Supplies, presence: true, length: {minimum: 10}
     validates :Directions, presence: true, length: {minimum: 15}
