d3a33baf57009cf9a585ce9fc5532b24b72089d6
Lucas Gordon <lgordo21@uncc.edu>
2018-11-28 01:03:35 +0000

    Add student adds a recipe scenario 2

--- Recipes/spec/integration/student_adds_a_recipe_spec.rb
+++ Recipes/spec/integration/student_adds_a_recipe_spec.rb
@@ -10,4 +10,16 @@ feature"Student adds a recipe" do
         expect(page).to have_field("Ingredients and supplies")
         expect(page).to have_field("Directions")
     end
+    
+    scenario "Student successfully creates a new recipe" do
+        visit new_article_path
+        expect(page).to have_content("New Recipe")
+        fill_in "Recipe name", with: "New Capybara Recipe, Insert Recipe Name Here"
+        fill_in "Ingredients and supplies", with: "This is a new Capybara recipe, It meets the desired length to be tested"
+        fill_in "Directions", with: "This is a test, Stir for ten seconds then add salt to taste"
+        click_button "Save"
+        expect(page).to have_content("New Capybara Recipe, Insert Recipe Name Here")
+        expect(page).to have_content("This is a new Capybara recipe, It meets the desired length to be tested")
+        expect(page).to have_content("This is a test, Stir for ten seconds then add salt to taste")
+    end
 end
